cfe556d5d0c5bf9eb7fffc7194e6b0f5097d935b
geekahmed <geekahmed1@gmail.com>
2022-06-29 22:57:04 +0200

    Running version

--- downloadmodel.py
+++ downloadmodel.py
@@ -5,10 +5,8 @@ import requests
 
 async def download_and_save_offensive_model() -> bool:
     try:
-        logger.info(f"Model 1 URL: ${MODEL1_URL}")
         response1 = requests.get(MODEL1_URL)
         open("classifier2_jlib", "wb").write(response1.content)
-        logger.info(f"Model 1 URL: ${MODEL2_URL}")
         response2 = requests.get(MODEL2_URL)
         open("vectroize2_jlib", "wb").write(response2.content)
         return True
